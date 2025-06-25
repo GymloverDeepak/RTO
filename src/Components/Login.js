b9d5a40323cb1a37dad1f5882f53f855ad2be181
Deepaksingh7010 <dksingh7010@gmail.com>
2025-06-25 10:47:16 +0530

    fix

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -15,7 +15,7 @@ export default function Login() {
 
   const users = [
     { email: "kapil", password: "kapil" },
-    { email: "deva", password: "deva" },
+    { email: "devan", password: "devan" },
     { email: "loki", password: "loki" },
   ];
 
