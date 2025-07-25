7a82ce2538cf6406653ca78136497519466fbe53
Deepaksingh7010 <dksingh7010@gmail.com>
2025-07-25 14:20:28 +0530

    password

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -14,9 +14,10 @@ export default function Login() {
   const [captchaInput, setCaptchaInput] = useState("");
 
   const users = [
-    { email: "kapil", password: "kapil" },
-    { email: "dev", password: "dev" },
-    { email: "loki", password: "loki" },
+    // { email: "kapil", password: "kapil" },
+    // { email: "dev", password: "dev" },
+    // { email: "loki", password: "loki" },
+     { email: "gym", password: "gymlover" },
   ];
 
   useEffect(() => {
